7506a2ccbe62c91e2bc39ad6b3ef8f9c99e3f669
mizu-ryo <mukusuke421@gmail.com>
2021-11-26 16:32:00 +0900

    [add]users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
       registrations: 'api/auth/registrations'
     }
   end
+  namespace 'api' do
+    namespace 'v1' do
+      resources :users, only: [:index]
+    end
+  end
 end
-
