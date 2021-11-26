cda693d208389ea9250d18031ae9f487f1fe4717
mizu-ryo <mukusuke421@gmail.com>
2021-11-26 16:40:25 +0900

    [add]seed_fu

---
+++ db/fixtures/user.rb
@@ -0,0 +1,17 @@
+User.seed( :id,
+    { id: 1 ,   name: 'mizu' ,
+                email: 'mizu@email.com' ,
+                password: 'mizumizu' ,
+                password_confirmation: 'mizumizu'
+    },
+    { id: 2 ,   name: 'ryo' ,
+                email: 'ryo@email.com' ,
+                password: 'ryoryo' ,
+                password_confirmation: 'ryoryo'
+    },
+    { id: 3 ,   name: 'aqua' ,
+                email: 'aqua@email.com' ,
+                password: 'aqua' ,
+                password_confirmation: 'aquaaqua' 
+    }
+)
